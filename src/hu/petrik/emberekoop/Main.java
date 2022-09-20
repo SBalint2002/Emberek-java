c189dcfa6da4e386e45273ff5b15a277e3151f87
Sárándi <sarandi.balint@gmail.com>
2022-09-20 13:57:11 +0200

    Adattagok felvétele + Main osztályban példányosítás

--- src/hu/petrik/emberekoop/Main.java
+++ src/hu/petrik/emberekoop/Main.java
@@ -1,2 +1,9 @@
-package hu.petrik.emberekoop;public class Main {
+package hu.petrik.emberekoop;
+import static java.lang.System.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Ember human = new Ember("Gipsz Jakab", "2001-1-1", "Budapest");
+        out.println(human);
+    }
 }
